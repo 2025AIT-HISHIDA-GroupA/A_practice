18955eb9e4621bdab492cd104ba3762d14541a97
KajitaTakaya <takaya0618.kjt@gmail.com>
2025-05-20 17:40:47 +0900

    テスト

--- modules/SayHello.py
+++ modules/SayHello.py
@@ -1,8 +1,8 @@
 class SayHello:
     def __init__(self, target="World"):
         self.target = target
-def say(self):
-    print(f"Hello, {self.target}!!")
+    def say(self):
+        print(f"Hello, {self.target}!!")
 
 if __name__ == '__main__':
     app = SayHello()
